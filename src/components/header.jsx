ee245e7112c653ef7b4f582d6429c77e82241cfb
León Cantillo <ldcantillom@gunimagdalena.edu.co>
2024-11-25 18:13:27 -0500

    [ADD] Header component.

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import './../styles/header.scss';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="header">
+                    <h1>PichiFly</h1>
+                    <ul className="navbar">
+                        <li><a href="/">Reservar</a></li>
+                        <li><a href="/reservations">Ofertas y destinos</a></li>
+                        <li><a href="/">Tu reserva</a></li>
+                    </ul>
+                    <a className="button type-3 login-button" href="/login">Login</a>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
